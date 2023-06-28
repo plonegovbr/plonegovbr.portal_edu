57192c527a1e761586030f85a7c70871ed44ce7c
Ramiro Batista da Luz <ramiroluz@gmail.com>
2023-06-28 18:18:34 -0300

    Corrige importa não utilizado.

--- src/plonegovbr/portal_edu/handlers.py
+++ src/plonegovbr/portal_edu/handlers.py
@@ -1,23 +1,9 @@
-from base64 import b64encode
 from plone import api
 from plone.distribution.core import Distribution
+from plonegovbr.portal_base.utils.handlers import ajusta_idioma
+from plonegovbr.portal_base.utils.handlers import converte_logo_data
 from plonegovbr.portal_edu import logger
 from Products.CMFPlone.Portal import PloneSite
-
-
-def ajusta_idioma(site: PloneSite, language: str = "pt-br"):
-    """Seta o idioma do site para pt-br."""
-    # Ajusta conteúdo
-    site.setLanguage(language)
-
-
-def converte_logo_data(logo: str):
-    """Converte os dados do logo de data-uri para o formato to registro do Plone."""
-    headers, data = logo.split("base64,")
-    filename = headers.split("name=")[1][:-1]
-    filenameb64 = b64encode(filename.encode("utf-8")).decode("utf-8")
-    response = f"filenameb64:{filenameb64};datab64:{data}"
-    return response.encode("utf-8")
 
 
 def post_handler(
